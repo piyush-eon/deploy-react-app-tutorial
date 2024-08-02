46ea9af58bb89afc9433dcf475c3ca2bfaf83681
Piyush Agarwal <piyushagarwalvo@gmail.com>
2024-08-02 16:14:00 +0530

    Final Code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Subscribe to RoadsideCoder
+      <p>Name = {import.meta.env.VITE_EXAMPLE}</p>
+    </div>
+  );
+}
+
+export default App;
